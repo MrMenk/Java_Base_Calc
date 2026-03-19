44f2a2f9bbd4f7e67f074dcea945cff5d08bf128
Mayank Niboriya <MrMenk@users.noreply.github.com>
2026-03-19 23:19:08 +0530

    initial commit

---
+++ src/main/java/com/myprojects/swing_learn/Swing_Learn.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.myprojects.swing_learn;
+
+/**
+ *
+ * @author My_Ma
+ */
+public class Swing_Learn {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
